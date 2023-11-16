9f953dfd5a1d7c93e32a8923e9f418230aeb8e5a
Bianca Sales <salesbyankaa@gmail.com>
2023-11-16 11:55:42 -0300

    Initial Commit

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 
-// Rotas
+// Routes
 $app->get('/users', '\Money\Controllers\ApiController:getAllUsers');
 $app->get('/users/{id}', '\MoneyProject\Controllers\ApiController:getUser');
 $app->post('/users', '\MoneyProject\Controllers\ApiController:createUser');
